fb28279c5b6de1d0b7ec4f5702ef191adece98a9
EC2 Default User <ec2-user@ip-172-31-19-52.us-east-2.compute.internal>
2024-08-06 18:09:37 +0000

    reduced metadata returned

--- janus/retrieval/retriever.py
+++ janus/retrieval/retriever.py
@@ -8,7 +8,10 @@ class Retriever:
         self.collection = self.client.get_collection(name=self.collection_name) 
 
     def retrieve(self, query : str, n_results : int = 3) -> list:
-        return self.collection.query(query_texts=[query], 
+        result = self.collection.query(query_texts=[query], 
                                      n_results=n_results, 
                                      include=['documents', 'metadatas'])
+        result_filenames = [entry['filename'] for entry in result['metadatas'][0]]
+        names_and_documents = tuple(zip(result_filenames, result['documents'][0]))
+        return names_and_documents
 
